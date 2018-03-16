065143b3e6891414fb48ef09fbb47cf499126988
ANASS EZ-ZOUAINE <ans-ezzouaine@hotmail.com>
2018-03-16 16:37:51 +0000

    wip

---
+++ vueapp/quasar.conf.js
@@ -0,0 +1,161 @@
+let path = require('path')
+// Configuration for your app
+module.exports = function (ctx) {
+  return {
+    // app plugins (/src/plugins)
+    plugins: [
+      'i18n',
+      'axios'
+    ],
+    css: [
+      'app.styl'
+    ],
+    extras: [
+      ctx.theme.mat ? 'roboto-font' : null,
+      'material-icons'
+      // 'ionicons',
+      // 'mdi',
+      // 'fontawesome'
+    ],
+    supportIE: true,
+    vendor: {
+      add: [],
+      remove: []
+    },
+    build: {
+      scopeHoisting: true,
+      vueRouterMode: 'history',
+      // gzip: true,
+      // analyze: true,
+      // extractCSS: false,
+      // useNotifier: false,
+      publicPath: '/',
+      htmlFilename: './../../resources/views/index.blade.php',
+      distDir: './../public/',
+      extendWebpack(cfg) {
+        cfg.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules|quasar)/
+        })
+        cfg.module.rules.push({
+          test: /\.(php)$/,
+          loader: 'ignore-loader'
+        })
+        cfg.module.rules.push({
+          test: /\.(html)$/,
+          loader: 'html-loader'
+        })
+        // cfg.context = path.resolve(__dirname, './src')
+        // cfg.resolve.alias['@'] = path.resolve(__dirname, './src')
+        // cfg.resolve.modules.push(path.resolve(__dirname, './Modules'))
+        // console.log(cfg.plugins)
+      }
+    },
+    devServer: {
+      // https: true,
+      // port: 8080,
+      open: false, // opens browser window automatically
+      proxy: {
+        // proxy all requests starting with /api to jsonplaceholder
+        '/api': {
+          // target: process.env.API_PROXY_URL,
+          target: 'http://fundev.xx',
+          changeOrigin: true,
+          pathRewrite: {
+            //   '^/api': '/api'
+          }
+        }
+      }
+    },
+    // framework: 'all' --- includes everything; for dev only!
+    framework: {
+      components: [
+        'QLayout',
+        'QLayoutHeader',
+        'QLayoutDrawer',
+        'QPageContainer',
+        'QPage',
+        'QToolbar',
+        'QToolbarTitle',
+        'QBtn',
+        'QIcon',
+        'QList',
+        'QListHeader',
+        'QItem',
+        'QItemMain',
+        'QItemSide'
+      ],
+      directives: [
+        'Ripple'
+      ],
+      // Quasar plugins
+      plugins: [
+        'Notify'
+      ]
+    },
+    // animations: 'all' --- includes all animations
+    animations: [],
+    pwa: {
+      cacheExt: 'js,html,css,ttf,eot,otf,woff,woff2,json,svg,gif,jpg,jpeg,png,wav,ogg,webm,flac,aac,mp4,mp3',
+      manifest: {
+        // name: 'Quasar App',
+        // short_name: 'Quasar-PWA',
+        // description: 'Best PWA App in town!',
+        display: 'standalone',
+        orientation: 'portrait',
+        background_color: '#ffffff',
+        theme_color: '#027be3',
+        icons: [
+          {
+            'src': 'statics/icons/icon-128x128.png',
+            'sizes': '128x128',
+            'type': 'image/png'
+          },
+          {
+            'src': 'statics/icons/icon-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png'
+          },
+          {
+            'src': 'statics/icons/icon-256x256.png',
+            'sizes': '256x256',
+            'type': 'image/png'
+          },
+          {
+            'src': 'statics/icons/icon-384x384.png',
+            'sizes': '384x384',
+            'type': 'image/png'
+          },
+          {
+            'src': 'statics/icons/icon-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png'
+          }
+        ]
+      }
+    },
+    cordova: {
+      // id: 'org.cordova.quasar.app'
+    },
+    electron: {
+      extendWebpack(cfg) {
+        // do something with cfg
+      },
+      packager: {
+        // OS X / Mac App Store
+        // appBundleId: '',
+        // appCategoryType: '',
+        // osxSign: '',
+        // protocol: 'myapp://path',
+
+        // Window only
+        // win32metadata: { ... }
+      }
+    },
+
+    // leave this here for Quasar CLI
+    starterKit: '1.0.0'
+  }
+}
